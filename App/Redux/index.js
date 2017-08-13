b9473c80bd942cbd1dee6f111ecb03293101f27c
jake <jake@vertaxe.com>
2017-08-13 11:54:51 +0200

    add login redux

--- App/Redux/index.js
+++ App/Redux/index.js
@@ -1,14 +1,15 @@
-import { combineReducers } from 'redux'
-import configureStore from './CreateStore'
-import rootSaga from '../Sagas/'
+import { combineReducers } from 'redux';
+import configureStore from './CreateStore';
+import rootSaga from '../Sagas/';
 
 export default () => {
   /* ------------- Assemble The Reducers ------------- */
   const rootReducer = combineReducers({
     nav: require('./NavigationRedux').reducer,
     github: require('./GithubRedux').reducer,
-    search: require('./SearchRedux').reducer
-  })
+    login: require('./LoginRedux').reducer,
+    search: require('./SearchRedux').reducer,
+  });
 
-  return configureStore(rootReducer, rootSaga)
-}
+  return configureStore(rootReducer, rootSaga);
+};
