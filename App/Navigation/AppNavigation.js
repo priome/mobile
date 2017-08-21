7b44d187c0f084c9b21ab8b6a495d78585af2417
jake <jake@vertaxe.com>
2017-08-21 18:45:06 +0200

    set LaunchScreen as default

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -13,7 +13,7 @@ const PrimaryNav = StackNavigator(
   {
     // Default config for all screens
     headerMode: 'none',
-    initialRouteName: 'Wizard',
+    initialRouteName: 'LaunchScreen',
     navigationOptions: {
       headerStyle: styles.header,
     },
